4ec62eb1fb1f427afba022b97f23104b2bcd9ce3
Hi-Timofey <Katkov0103@yandex.ru>
2020-11-04 16:39:10 +1000

    Finished basic logic for working with config of the program

--- whistle_bristle/utils/config_manager.py
+++ whistle_bristle/utils/config_manager.py
@@ -9,11 +9,21 @@ class ConfigError(Exception):
 # TODO Make syntax checher for config file
 class ConfigManager():
 
+    DATABASE_PATH = 'database_path'
+
     def __init__(self, project_dir):
         self.project_dir = project_dir
         self.cfgfile_name = 'config.txt'
         self.cfgfile_path = self.project_dir + self.cfgfile_name
         self._init_cfg_file()
+
+        self.DEFAULT_CONFIG = {
+            'database_path': f'{self.project_dir}whistle_bristle/db/files.db'
+            }
+
+    def show_info(self):
+        print(
+            f'Config file "{self.cfgfile_name}":\n\tIs empty: {self.is_blank_cfg()}\n\tPath to: {self.cfgfile_path}')
 
     def _init_cfg_file(self):
         '''Creates config file if it is not exists.'''
@@ -29,7 +39,21 @@ class ConfigManager():
         return True
 
     def set_default(self):
-        print(self.project_dir)
+        '''Setting config file to the default state'''
 
-        # with open(self.cfgfile_path, 'w') as cfgfile:
-        #     cfgfile.write(f'database_path = {self}' + '\n')
+        with open(self.cfgfile_path, 'w') as cfgfile:
+            for cfg in self.DEFAULT_CONFIG:
+                cfgfile.write(f'{cfg}={self.DEFAULT_CONFIG[cfg]}' + '\n')
+
+    def get_cfg_value(self, key):
+        '''Get some value from config'''
+
+        with open(self.cfgfile_path, 'r') as cfgfile:
+            cfg = cfgfile.readlines()
+            for line in cfg:
+                l = line.strip()
+                if l[0] != '#' and '=' in l:
+                    name, value = l.split('=')
+                    if name == key:
+                        # print(f'"{name}" was found! value="{value}"')
+                        return value
--- whistle_bristle_cli.py
+++ whistle_bristle_cli.py
@@ -18,8 +18,6 @@ if cfg_manager.is_blank_cfg():
     if answer.lower() == 'y':
         cfg_manager.set_default()
 
-input('STOP IT')
-
 
 def create_parser():
     # TODO: Set description for a parser
@@ -37,23 +35,18 @@ def create_parser():
     bristle_parser = subparsers.add_parser('bristle')
     bristle_actions = bristle_parser.add_mutually_exclusive_group()
 
-    # whistle_parser = subparsers.add_parser('whistle')
-
     bristle_actions.add_argument('-fo', '--filesonly',
                                  nargs='+',
                                  type=check_path,
                                  default=False,
                                  help=files_help
                                  )
-
     bristle_actions.add_argument('-da', '--deleteall',
                                  action='store_true',
                                  default=False)
-
     bristle_actions.add_argument('-l', '--listfiles',
                                  action='store_true',
                                  default=False)
-
     bristle_actions.add_argument('-fp', '--filespriority',
                                  nargs='+',
                                  type=check_path_and_priority,
@@ -73,6 +66,18 @@ def create_parser():
                                  help=changing_priority_help
                                  )
 
+    config_parser = subparsers.add_parser('config')
+    config_actions = config_parser.add_mutually_exclusive_group()
+    config_actions.add_argument('-i', '--info',
+                                action='store_true',
+                                default=False,
+                                help='Show information about config file')
+    config_actions.add_argument('-db', '--pathtodb',
+                                action='store_true',
+                                default=False,
+                                help='Show files database path')
+
+    # whistle_parser = subparsers.add_parser('whistle')
     # whistle_parser.add_argument('-k', "--key", '--keycombo',
     #                     type=check_key,
     #                     default='<ctrl>+<alt>+e',
@@ -86,8 +91,7 @@ def whistle_working(args):
 
 
 def bristle_working(args):
-    # TODO: Not correct selecting path for database
-    path = '/home/katok/combat/python_workspace/whistle_bristle/whistle_bristle/db/files.db'
+    path = cfg_manager.get_cfg_value(ConfigManager.DATABASE_PATH)
     db = files.FilesDB(path)
 
     if args.delete:
@@ -116,12 +120,24 @@ def bristle_working(args):
             print('There are no files in database to erase!')
 
 
+def config_working(args):
+    if args.pathtodb:
+        # TODO: Can also change path to database
+        path_to_db = cfg_manager.get_cfg_value(ConfigManager.DATABASE_PATH)
+        print(
+            f'Path to database: {path_to_db}')
+    if args.info:
+        cfg_manager.show_info()
+
+
 if __name__ == '__main__':
 
     parser = create_parser()
     args = parser.parse_args(sys.argv[1:])
 
-    if args.cmd_type == 'bristle':
+    if args.cmd_type == 'config':
+        config_working(args)
+    elif args.cmd_type == 'bristle':
         bristle_working(args)
-    elif args.cmd_type == 'whistle':
-        whistle_working(args)
+    # elif args.cmd_type == 'whistle':
+    #     whistle_working(args)
